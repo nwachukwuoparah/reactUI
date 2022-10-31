6b3d7a7b175c4bb6edec32f93652c18395aafae0
Oparah <nwachukwuoparah@gmail.com>
2022-10-31 11:14:37 +0100

    changes at footer link

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import App2 from './App2';
-import Footer from './Footer'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import App2 from "./App2";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <App2 />
-    {/* <Footer /> */}
   </React.StrictMode>
 );
 
